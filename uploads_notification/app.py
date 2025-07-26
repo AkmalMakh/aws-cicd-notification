24d41eb9a9e7dc41c255524e02662fd9f39c31f3
akmalmakhmudov <akmalmakhmudov@MacBook-Pro-3.local>
2025-07-26 14:32:13 +0900

    Adding Policy 1

--- uploads_notification/app.py
+++ uploads_notification/app.py
@@ -10,7 +10,7 @@ def lambda_handler(event, context):
 
     records = event.get('Records', [])
     if not records:
-        print("No records found.")
+        print("No record found.")
         return {'statusCode': 200, 'body': 'No records'}
 
     for record in records:
