beef7566bcf8ebc53858e6d0ddfd0c5ac7f06606
akmalmakhmudov <akmalmakhmudov@MacBook-Pro-3.local>
2025-07-26 11:35:31 +0900

    Re-trigger

--- uploads_notification/app.py
+++ uploads_notification/app.py
@@ -10,7 +10,7 @@ def lambda_handler(event, context):
 
     records = event.get('Records', [])
     if not records:
-        print("No records to found.")
+        print("No records found.")
         return {'statusCode': 200, 'body': 'No records'}
 
     for record in records:
