a0228fa8b31259ae62d8c549b59588824100fbf5
akmalmakhmudov <akmalmakhmudov@MacBook-Pro-3.local>
2025-07-26 11:56:27 +0900

    Re-trigger 4

--- uploads_notification/app.py
+++ uploads_notification/app.py
@@ -25,7 +25,7 @@ def lambda_handler(event, context):
                 Message=message,
                 Subject="New Upload Notifications"
             )
-            print("✅ Message published to SNS")
+            print("✅ Message published to SNS check it")
 
         except Exception as e:
             print("❌ Error processing record:", str(e))
